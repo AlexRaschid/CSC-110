cf8ad88eec31c2688317f9fe7e2ebbcbdeff967e
Alexander Raschid <alexraschid1337@gmail.com>
2017-02-26 03:58:06 -0500

    Initial commit/ Completed the work

---
+++ Assignment 1/src/Assignment.java
@@ -0,0 +1,46 @@
+
+public class Assignment {
+	public static void main(String[] args){
+		//Declare an integer variable and initialize to 10. 
+		int num1 = 10;
+		//Declare an float variable and initialize to 2. 
+		float num2 = 2;
+		
+		//Using the combined assignment operator, increment the integer variable by 3
+		num1 += 3;
+		
+		
+		// Print the following statement. 
+		// variable 1 % variable 2 = 13 % 2.0 = 1.0
+		System.out.println("variable 1 % variable 2 = " + num1 + " % " + num2 + " = " + (num1 % num2));
+		
+		
+		
+		System.out.println(); //For a space
+		
+		
+		
+		//Write the proper print statements to print the following
+		/*
+		  	\n Advances the cursor to the next line for subsequent printing
+			\t Causes the cursor to skip over to the next tab stop
+			\b Causes the cursor to back up, or move left, one position
+			\\ Causes a backslash to be printed 
+		 */
+		
+		System.out.println("\\n Advances the cursor to the next line for subsequent printing");
+		System.out.println("\\t Causes the cursor to skip over to the next tab stop  ");
+		System.out.println("\\b Causes the cursor to back up, or move left, one position  ");
+		System.out.print("\\\\ Causes a backslash to be printed");
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+}
