3ef0a973fe4888322f05ae02dc405ea08a8b2d10
Alexander Raschid <alexraschid1337@gmail.com>
2017-03-11 00:09:57 -0500

    Initial Assignment 2 commit, attempted but not fully complete

--- Assignment 2/src/assignmentTwo.java
+++ Assignment 2/src/assignmentTwo.java
@@ -1,3 +1,6 @@
+//Code Written by Alexander E. Raschid
+
+
 import java.util.*;
 import javax.swing.*;
 
@@ -5,14 +8,19 @@ import javax.swing.*;
 public class assignmentTwo {
 
 	public static void main(String[] args) {
-
+		//New Scanner
 		Scanner console = new Scanner(System.in);
+		//Asks for input and stores input in userInput
 		System.out.println("Type in something");
 		String userInput = console.nextLine();
 		
-		int firstS = userInput.indexOf('s');
-		int secondS = findSecondS(userInput, firstS);
+		//Finds the 2nd s value
+		int secondS = findSAgain(userInput);
+		//Finds the 10 letters after mid
+		//String tenLettersAfterMid = firstTenAfterMid(userInput);
+
 		
+		//The message dialog
 		JOptionPane.showMessageDialog(null,"Information:\n" +
 		"Line entered: " + userInput + "\n" +
 		"UpperCase: " + userInput.toUpperCase() + "\n"+
@@ -20,29 +28,53 @@ public class assignmentTwo {
 		"Length: " + userInput.length() + "\n" +
 		"First Letter: " + userInput.charAt(0) + "\n" +
 		"Last Letter: " + userInput.charAt(userInput.length()-1) + "\n"+
-		"First index of \"s\": " + firstS + "\n" +
-		"Second index of \"s\": " + secondS + " (If -1, then there is no 2nd s)\n" +
-		" ");
+		"First index of \"s\": " + userInput.indexOf('s') + " (If -1, then there is no 1nd, s)" + "\n" +
+		"Second index of \"s\": " + secondS  + " (If -1, then there is no 2nd, s)\n" +
+		"10 Characters starting : " + userInput.substring((userInput.length()/2), ((userInput.length()/2) + 10)));
 			
 	}
 	
 	
-	public static int findSecondS(String userInput, int firstS){
-		String userInputLower = userInput.toLowerCase();
+	//Find the 2nd s value
+	public static int findSAgain(String userInput) {
+		//Loops through the string
 		for(int i = 0; i < userInput.length(); i++){
-			if(userInputLower.indexOf(i) == 's' && userInputLower.indexOf(i) != firstS){
-				return userInputLower.indexOf(i);
+			//Makes sure that the s value detected is not
+			//The first s value
+			if(userInput.charAt(i) == 's' && userInput.indexOf('s') != i){
+				//returns the 2nd s index value
+				return i;	
 			}
 		}
 		
-		while(firstS != -1){
-			
-			
-		}
-			return -1;
-		
-		
+		// if no value detected, return -1
+		return -1;
 	}
+	
+	
+	
+//	//Attempts at finding 10 characters after mid char
+//	//By starting in middle and increments only 10 values.
+//	
+//	public static String firstTenAfterMid(String userInput){
+//		String tenLetters = "";
+//		//By starting in middle, thats why i is equal to about half the string length,
+//		//and increments only 10 values, since it starts in mid but only goes up
+//		// 10 more.
+//		for(int i = (userInput.length()/2); i < (userInput.length()/2)+10; i++){
+//			//Updating the String variable
+//			tenLetters += userInput.charAt(i);
+//		}
+//		
+//		//returning the string.
+//		return tenLetters;
+//		
+//		
+//		
+//		
+//	}
+	
+	
 	
 	
 
